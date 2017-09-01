c6bb4d04867a8243dd44fee4c8b48c703972c0f3
Daniel Pigford <daniel@Daniels-Air.pigford.loc>
2017-09-01 08:39:25 -0400

    Initial version

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace blade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
